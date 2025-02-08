7adc6f09a44d08ffc91668996e31a62ca177e7ab
Kshitij <kshitij.vijay.pawar@gmail.com>
2025-02-08 13:25:33 +0530

    ``` No changes made ```

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Page1 from "./pages/page1";
 import Header from "./components/header";
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const App = () => {
   return (
@@ -12,7 +12,6 @@ const App = () => {
           <Page1 />
         </div>
       </HashRouter>
-     
     </>
   );
 };
